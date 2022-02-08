8f644ecc5371217396a4833868efce33ebcdf2b8
Franco Moyano <koprammoyano@gmail.com>
2022-02-07 21:48:34 -0300

    Commit de prueba

--- src/main/java/egg/libreria/LibreriaApplication.java
+++ src/main/java/egg/libreria/LibreriaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibreriaApplication {
 
 	public static void main(String[] args) {
+		//hola
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
 
